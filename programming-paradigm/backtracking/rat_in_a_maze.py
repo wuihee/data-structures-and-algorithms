0c872daba82b662f6e95f082b9b3cb8119d20cfd
Wuihee <wuihee@gmail.com>
2022-03-12 19:18:32 +0800

    Updated rat in a maze problem.

--- programming-paradigm/backtracking/rat_in_a_maze.py
+++ programming-paradigm/backtracking/rat_in_a_maze.py
@@ -7,6 +7,80 @@ right corner. The rat can only move rightwards and downwards.
 """
 
 
-def solve_maze(maze):
-    """Hi."""
-    pass
+def solve_maze(maze, solution_maze, row, col):
+    """
+    Recursive function to solve rat in a maze problem.
+
+    Parameters
+    ----------
+    maze : Matrix representing the maze.
+    solution_maze : Maps current path that has been travelled so far.
+    row : Current y position in the maze.
+    col : Current x position in the maze.
+    """
+
+    N = len(maze)
+    
+    # Base Case: End point reached.
+    if row == N - 1 and col == N - 1:
+        return True
+
+    # Try going right.
+    if col < N and maze[row][col + 1] == 1:
+
+        # Indicate that rat has travelled right in the solution.
+        solution_maze[row][col + 1] = 1
+
+        # Recursively check if the next move after moving right leads to a
+        # solution.
+        if solve_maze(maze, solution_maze, row, col + 1):
+            return True
+
+        # If no solution was found, backtrack.
+        solution_maze[row][col + 1] = 0
+
+    # Try going down.
+    if row < N and maze[row + 1][col] == 1:
+
+        # Indicate that rat has travelled down in the solution.
+        solution_maze[row + 1][col] = 1
+
+        # Recursively check if the next move after moving down leads to a
+        # solution.
+        if solve_maze(maze, solution_maze, row + 1, col):
+            return True
+
+        # If no solution was found, backtrack.
+        solution_maze[row + 1][col] = 0
+
+    # Neither going right or down finds a solution.
+    return False
+
+
+def solve_maze_aux(maze):
+    """
+    Auxilliary function to the main recursive solution.
+
+    Parameters
+    ----------
+    maze : Matrix representing the maze.
+    """
+
+    N = len(maze)
+    solution_maze = [[0 for _ in range(N)] for _ in range(N)]
+
+    # Start at 0,0.
+    solution_maze[0][0] = 1
+
+    if solve_maze(maze, solution_maze, 0, 0):
+        for i in solution_maze:
+            print(i)
+    else:
+        print('No solution found.')
+
+
+maze = [[1, 0, 0, 0],
+        [1, 1, 0, 1],
+        [0, 1, 0, 0],
+        [1, 1, 1, 1]]
+solve_maze_aux(maze)

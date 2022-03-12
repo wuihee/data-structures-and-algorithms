a833667807cd5984c54a67e09a45ffeabd6043f2
Wuihee <wuihee@gmail.com>
2022-03-12 10:13:53 +0800

    Added rat in a maze problem.

---
+++ programming-paradigm/backtracking/rat_in_a_maze.py
@@ -0,0 +1,12 @@
+"""
+Rat in a Maze
+
+Given an N x N maze matrix filled with 0s and 1s, where a 0 represents a dead
+end and 1 represents a path, find a path from the top left corner to the bottom
+right corner. The rat can only move rightwards and downwards.
+"""
+
+
+def solve_maze(maze):
+    """Hi."""
+    pass

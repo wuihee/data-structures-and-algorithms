b5afca03edbc7f0f07c55616c1355bbdb8db05fd
wuihee <wuihee@gmail.com>
2023-04-21 22:07:16 +0800

    Updated 0-1 knapsack

--- programming-paradigm/dynamic_programming/classic_problems/knapsack_0-1.py
+++ programming-paradigm/dynamic_programming/classic_problems/knapsack_0-1.py
@@ -1,16 +1,26 @@
 """
 0-1 Knapsack
+
+We are given N items where each item has some weight and profit associated with
+it. We are also given a bag with capacity W. The target is to put the items
+into the bag such that the sum of profits associated with them is the maximum
+possible.
+
+The constraint here is we can either put an item completely into the bag or
+cannot put it at all, this is why the problem is called 0-1.
 """
 
 
-def knapsack(W, items):
+def knapsack(W: int, items: int) -> int:
     """
     Bottom-up dynamic programming solution.
 
-    Parameters
-    ----------
-    W : Capacity of knapsack.
-    items : Each item contains (weight, value).
+    Args:
+        W (int): Capacity of knapsack.
+        items (list[int]): Each item contains (weight, value).
+
+    Returns:
+        int: Returns the maximum possible value of items.
     """
 
     n = len(items)
@@ -18,7 +28,7 @@ def knapsack(W, items):
 
     for i in range(1, n + 1):
         for w in range(1, W + 1):
-            
+
             # Option 1: don't include ith item.
             dp[i][w] = dp[i - 1][w]
 
